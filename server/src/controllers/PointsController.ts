ea18a17e631b57505e0306eba51a5aa4d983850a
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-06-27 18:41:35 -0300

    Improvements on server and web

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,14 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return response.json({ points })
+    const serielizedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `${process.env.SERVER_URL}/uploads/${point.image}`,
+      };
+    });
+
+    return response.json({ points: serielizedPoints })
   }
 
   async show(request: Request, response: Response) {
@@ -34,7 +41,12 @@ class PointsController {
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+    const serielizedPoint = {
+        ...point,
+        image_url: `${process.env.SERVER_URL}/uploads/${point.image}`,
+    };
+
+    return response.json({ point: serielizedPoint, items });
   }
 
   async create(request: Request, response: Response) {
@@ -43,7 +55,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -57,12 +69,15 @@ class PointsController {
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      }
-    })
+    const pointItems = items
+      .split(',')
+      .map((item: string) => parseInt(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        }
+      });
 
     await trx('point_items').insert(pointItems);
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import cors from 'cors';
 import express from 'express';
+import { errors } from 'celebrate';
 import { config } from 'dotenv';
 import routes from './routes';
 
@@ -12,5 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(process.env.SERVER_PORT);
